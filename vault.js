c6f65d6d019ff2f1cd23e00c49b75be995d859b0
Janelle K Hirano <jaahnelle@gmail.com>
2019-07-03 21:51:22 -1000

    finished

--- vault.js
+++ vault.js
@@ -1,4 +1,25 @@
-'use strict';
+"use strict";
 module.exports = function() {
-  
+  var myVault = {};
+
+  function setValue(key, value) {
+    myVault[key] = value;
+    console.log(">>>>>>", myVault);
+    // console.log(value)
+
+    return myVault[key];
+  }
+
+  function getValue(key) {
+    if (myVault.hasOwnProperty(key)) {
+      return myVault[key];
+    } else {
+      return null;
+    }
+  }
+
+  return {
+    setValue,
+    getValue
+  };
 };
